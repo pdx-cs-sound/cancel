924bc8ffefb508f24f2cbbb3284e893d52cb62f6
Bart Massey <bart.massey@gmail.com>
2020-04-29 18:33:46 -0700

    fixed bug; added modulation option

--- cancel.py
+++ cancel.py
@@ -1,8 +1,6 @@
 # Audio Cancellation Demo
 # Bart Massey
 
-# https://www.uaudio.com/blog/audio-compression-basics/
-
 import argparse
 import numpy as np
 import soundfile
@@ -12,21 +10,38 @@ samplerate = 48000
 ap = argparse.ArgumentParser()
 ap.add_argument(
     "-f", "--freq",
-    help="Fundamental frequency.",
+    help="Fundamental frequency in Hz.",
     type=float,
     default=1000.0,
 )
 ap.add_argument(
     "-t", "--time",
-    help="Total time.",
+    help="Total time in secs.",
     type=float,
     default=1.0,
 )
 ap.add_argument(
-    "-o", "--offset",
-    help="Offset time.",
+    "--mod_offset",
+    help="Offset time as fraction of cycle.",
     type=float,
-    default=0.001,
+    default=0.0,
+)
+ap.add_argument(
+    "--mod-freq",
+    help="Modulation frequency in Hz.",
+    type=float,
+)
+ap.add_argument(
+    "--mod-range",
+    help="Modulation range as fraction of cycle.",
+    type=float,
+    default=1.0,
+)
+ap.add_argument(
+    "--mod-depth",
+    help="Modulation depth as fraction of full amplitude.",
+    type=float,
+    default=1.0,
 )
 ap.add_argument(
     "outfile",
@@ -35,23 +50,35 @@ ap.add_argument(
 args = ap.parse_args()
 
 # Sine
+nsamples = int(samplerate * args.time)
 ts = np.linspace(
     0,
-    2 * np.pi * args.freq * samplerate * args.time,
-    int(args.time * samplerate),
+    2 * np.pi * args.freq * args.time,
+    nsamples,
 )
-psignal = 0.5 * np.sin(ts)
+mod_offset = 2 * np.pi * args.mod_offset
+tms = ts + mod_offset
+if args.mod_freq is not None:
+    mod_range = args.mod_range * np.pi
+    print(mod_range)
+    tm = np.linspace(
+        0,
+        2 * np.pi * args.mod_freq * args.time,
+        nsamples,
+    )
+    tms += mod_range * np.sin(tm)
+ssignal = np.sin(ts)
+msignal = args.mod_depth * np.sin(tms)
+outsignal = 0.5 * (ssignal + msignal)
 
-offset = int(samplerate * args.offset)
-earlysignal = np.append(psignal, np.zeros(offset))
-latesignal = np.append(np.zeros(offset), psignal)
-outsignal = earlysignal + latesignal
+def write_wave(name, sig):
+    outfile = open(name, "wb")
+    soundfile.write(
+        outfile,
+        sig,
+        samplerate,
+        subtype='PCM_16',
+        format='WAV',
+    )
 
-outfile = open(args.outfile, "wb")
-soundfile.write(
-    outfile,
-    outsignal,
-    samplerate,
-    subtype='PCM_16',
-    format='WAV',
-)
+write_wave(args.outfile, outsignal)
